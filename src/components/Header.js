5d0f25e4ab40629f78cdcd6d9da13c057d56e11b
Alberto <albertoavv@hotmail.es>
2020-02-09 23:12:41 -0400

    styles with JavaScript
    TextoHeader

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,17 @@ const ContenedorHeader = styled.header`
     color: #ffffff;
 `;
 
+const TextoHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`;
+
 const Header = ({titulo}) => {
     return (
         <ContenedorHeader>
-            <h1>{titulo}</h1>
+            <TextoHeader>{titulo}</TextoHeader>
         </ContenedorHeader>
     );
 };
